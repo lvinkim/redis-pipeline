0cd93141c67d9a4d482d667eb061bfe66cdc2844
Vinkim Lee <lee.vinkim@yahoo.com>
2018-04-11 00:12:30 +0800

    first commit

---
+++ app/main.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lvinkim
+ * Date: 11/04/2018
+ * Time: 12:08 AM
+ */
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+// load env
+$env = new Dotenv(__DIR__.'/../');
+$env->load();
+

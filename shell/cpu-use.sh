e531c9b0f676dd9b2f8cfd2db95638c57cd79d0c
Vinkim Lee <lee.vinkim@yahoo.com>
2018-04-12 23:57:49 +0800

    [feature] 添加服务器资源使用监控脚本 3

--- shell/cpu-use.sh
+++ shell/cpu-use.sh
@@ -7,4 +7,4 @@ app="server"
 item="cpu_use"
 value=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f\n", prefix, 100 - v }');
 
-echo "{\"host\":\"${host}\",\"app\":\"${app}\",\"item\":\"${item}\",\"value\":\"${value}\"}" >> ${basepath}/../var/logstash/health_watcher.log;
+echo "{\"host\":\"${host}\",\"app\":\"${app}\",\"item\":\"${item}\",\"value\":\"${value}\"}" >> ${basepath}/../var/logstash/health-watcher.log;
--- shell/disk-use.sh
+++ shell/disk-use.sh
@@ -7,4 +7,4 @@ app="server"
 item="disk_use"
 value=$(df -hl | awk 'BEGIN {max = 0} {if ($5+0 > max+0) max=$5} END {printf "%3.0f", max}');
 
-echo "{\"host\":\"${host}\",\"app\":\"${app}\",\"item\":\"${item}\",\"value\":\"${value}\"}" >> ${basepath}/../var/logstash/health_watcher.log;
+echo "{\"host\":\"${host}\",\"app\":\"${app}\",\"item\":\"${item}\",\"value\":\"${value}\"}" >> ${basepath}/../var/logstash/health-watcher.log;

d8a8a00ebfc3106b6fac9cdff12d1f7f6f1b765b
Lee Vinkim <vinkim@xiyanghui.com>
2018-04-17 13:36:15 +0800

    [feature] cpu-use.sh memory-use.sh

--- shell/cpu-use.sh
+++ shell/cpu-use.sh
@@ -5,6 +5,20 @@ basepath=$(cd `dirname $0`; pwd)
 host=$(hostname);
 app="server"
 item="cpu_use"
-value=$(top -b -n2 -p 1 | fgrep "Cpu(s)" | tail -1 | awk -F'id,' -v prefix="$prefix" '{ split($1, vs, ","); v=vs[length(vs)]; sub("%", "", v); printf "%s%.1f\n", prefix, 100 - v }');
+
+CPU_1=$(cat /proc/stat | grep 'cpu ' | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8}')
+SYS_IDLE_1=$(echo $CPU_1 | awk '{print $4}')
+Total01=$(echo $CPU_1 | awk '{printf "%.f",$1+$2+$3+$4+$5+$6+$7}')
+sleep 2
+CPU_2=$(cat /proc/stat | grep 'cpu ' | awk '{print $2" "$3" "$4" "$5" "$6" "$7" "$8}')
+SYS_IDLE_2=$(echo $CPU_2 | awk '{print $4}')
+Total_2=$(echo $CPU_2 | awk '{printf "%.f",$1+$2+$3+$4+$5+$6+$7}')
+SYS_IDLE=`expr $SYS_IDLE_2 - $SYS_IDLE_1`
+Total=`expr $Total_2 - $Total01`
+TT=`expr $SYS_IDLE \* 100`
+SYS_USAGE=`expr $TT / $Total`
+SYS_Rate=`expr 100 - $SYS_USAGE`
+
+value=${SYS_Rate};
 
 echo "{\"host\":\"${host}\",\"app\":\"${app}\",\"item\":\"${item}\",\"value\":${value}}" >> ${basepath}/../var/logstash/health-watcher.log;
--- shell/memory-use.sh
+++ shell/memory-use.sh
@@ -5,6 +5,6 @@ basepath=$(cd `dirname $0`; pwd)
 host=$(hostname);
 app="server"
 item="memory_use"
-value=$(free -m|grep Mem|awk '{printf "%2.0f", 100*($3-$6-$7)/$2}');
+value=$(free -m | awk 'NR==2{printf "%3.0f", $3*100/$2 }');
 
 echo "{\"host\":\"${host}\",\"app\":\"${app}\",\"item\":\"${item}\",\"value\":${value}}" >> ${basepath}/../var/logstash/health-watcher.log;
